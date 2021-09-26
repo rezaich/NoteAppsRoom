6a3eae264cd2bc413281d5d08583fe3801f0c180
rezaich <ichsanireza@gmail.com>
2021-09-26 20:24:21 +0700

    add NoteAdapter.kt and menu on action bar for delete action

--- app/src/main/java/com/zaich/noteappsroom/database/NoteDao.kt
+++ app/src/main/java/com/zaich/noteappsroom/database/NoteDao.kt
@@ -3,6 +3,7 @@ package com.zaich.noteappsroom.database
 import androidx.lifecycle.LiveData
 import androidx.room.*
 
+@Dao
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(note: Note)
--- app/src/main/java/com/zaich/noteappsroom/ui/main/MainActivity.kt
+++ app/src/main/java/com/zaich/noteappsroom/ui/main/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.zaich.noteappsroom.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.zaich.noteappsroom.R
+import com.zaich.noteappsroom.ViewModelFactory
+import com.zaich.noteappsroom.database.Note
+import com.zaich.noteappsroom.databinding.ActivityMainBinding
+import com.zaich.noteappsroom.ui.insert.NoteAddUpdateActivity
 
 class MainActivity : AppCompatActivity() {
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+    private lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this, {
+            if (it != null) {
+                adapter.setListNotes(it as ArrayList<Note>)
+            }
+        })
+
+        adapter = NoteAdapter()
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener { view ->
+            if (view.id == R.id.fab_add) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
     }
 }
